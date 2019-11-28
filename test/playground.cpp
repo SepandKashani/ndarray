bf95b68f195ca5631ffbc8427adff1d118c0ba28
Sepand KASHANI <kashani.sepand@gmail.com>
2019-11-28 17:53:40 +0100

    Add playground for small exploratory tests.

---
+++ test/playground.cpp
@@ -0,0 +1,16 @@
+// ############################################################################
+// playground.cpp
+// ==============
+// Author : Sepand KASHANI [kashani.sepand@gmail.com]
+// ############################################################################
+
+#ifndef PLAYGROUND_CPP
+#define PLAYGROUND_CPP
+
+#include "ndarray/ndarray.hpp"
+
+int main(void) {
+    return 0;
+}
+
+#endif // PLAYGROUND_CPP
