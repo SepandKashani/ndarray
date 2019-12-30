91dc17f7264aae07f003b8577d7baada0774fe99
Sepand KASHANI <kashani.sepand@gmail.com>
2019-12-30 16:22:25 +0100

    Implement nd::linalg::mm()

--- ndarray/_ndlinalg.hpp
+++ ndarray/_ndlinalg.hpp
@@ -7,6 +7,13 @@
 #ifndef _NDLINALG_HPP
 #define _NDLINALG_HPP
 
+#include <algorithm>
+#include <sstream>
+
+#include "_ndarray.hpp"
+#include "_ndtype.hpp"
+#include "_ndutil.hpp"
+
 namespace nd { template <typename T> class ndarray; }
 
 namespace nd::linalg {
@@ -18,22 +25,64 @@ namespace nd::linalg {
      * A : ndarray<T> const&
      *     (a[0], ..., a[N-2], d) array.
      * B : ndarray<T> const&
-     *     (d, b[0], ..., b[M-2]) array.
+     *     (d, b[1], ..., b[M-1]) array.
+     * out : ndarray<T>* const
+     *     Optional buffer to store result.
+     *     Must have the same dimensions as the output, and be contiguous.
      *
      * Returns
      * -------
      * C : ndarray<T>
-     *     (a[0], ..., a[N-2], b[0], ..., b[M-2]) array.
+     *     (a[0], ..., a[N-2], b[1], ..., b[M-1]) array.
      *
      * Examples
      * --------
-     * Let A \in \bR^{5 \times 3 \times 4} and B \in \bR^{4, 7, 2}.
-     * Then C = mm(A, B) \in \bR^{5 \times 3 \times 7 \times 2} such that
+     * Let A \in \bR^{5, 3, 4} and B \in \bR^{4, 7, 2}.
+     * Then C = mm(A, B) \in \bR^{5, 3, 7, 2} such that
      *     C[i,j,k,l] = \sum_{q = 0}^{3} A[i,j,q] B[q,k,l]
      */
     template <typename T>
     ndarray<T> mm(ndarray<T> const& A, ndarray<T> const& B, ndarray<T>* const out = nullptr) {
-        return A + B;
+        static_assert(is_int<T>() || is_float<T>() || is_complex<T>(),
+                      "Only {int, float, complex} types allowed.");
+
+        shape_t const& sh_A = A.shape();
+        shape_t const& sh_B = B.shape();
+
+        { // Validate shapes
+            bool const correct_shape = sh_A[A.ndim() - 1] == sh_B[0];
+            std::stringstream error_msg;
+            error_msg << "Cannon multiply arrays of shape {"
+                      << sh_A << ", " << sh_B << "}. \n";
+            util::NDARRAY_ASSERT(correct_shape, error_msg.str());
+        }
+
+        size_t const sh_Ar0 = std::accumulate(sh_A.begin(), sh_A.end() - 1, size_t(1), std::multiplies<size_t>());
+        size_t const sh_Ar1 = sh_A[A.ndim() - 1];
+        auto Ar = A.reshape({sh_Ar0, sh_Ar1});
+        auto const eAr = util::interop::aseigenarray<T, mapM_t<T>>(&Ar);
+
+        size_t const sh_Br0 = sh_B[0];
+        size_t const sh_Br1 = std::accumulate(sh_B.begin() + 1, sh_B.end(), size_t(1), std::multiplies<size_t>());
+        auto Br = B.reshape({sh_Br0, sh_Br1});
+        auto const eBr = util::interop::aseigenarray<T, mapM_t<T>>(&Br);
+
+        shape_t sh_C(std::max<size_t>(A.ndim() + B.ndim() - 2, 1), 1);
+        std::copy_n(sh_A.cbegin(), A.ndim() - 1, sh_C.begin());
+        std::copy_n(sh_B.crbegin(), B.ndim() - 1, sh_C.rbegin());
+        if(out != nullptr) {
+            std::stringstream error_msg;
+            error_msg << "Parameter[out]: Expected " << sh_C << " array, "
+                      << "got " << out->shape() << ".\n";
+            util::NDARRAY_ASSERT(sh_C == out->shape(), error_msg.str());
+            util::NDARRAY_ASSERT(out->is_contiguous(), "Parameter[out] must point to a contiguous array.");
+        }
+        ndarray<T> C = (out == nullptr) ? ndarray<T>(sh_C) : *out;
+        auto Cr = C.reshape({sh_Ar0, sh_Br1});
+        auto eCr = util::interop::aseigenarray<T, mapM_t<T>>(&Cr);
+
+        (*eCr) = (*eAr) * (*eBr);
+        return C;
     }
 
     /*
--- test/test_ndlinalg.cpp
+++ test/test_ndlinalg.cpp
@@ -7,11 +7,285 @@
 #ifndef TEST_NDLINALG_CPP
 #define TEST_NDLINALG_CPP
 
+#include <stdexcept>
+
 #include <gtest/gtest.h>
 #include "test_type.hpp"
 
 #include "ndarray/ndarray.hpp"
 
-namespace nd::linalg {}
+namespace nd::linalg {
+    template <typename T> class TestNdLinalgIntFloatComplex : public ::testing::Test {};
+    TYPED_TEST_CASE_P(TestNdLinalgIntFloatComplex);
+
+    TYPED_TEST_P(TestNdLinalgIntFloatComplex, TestMM) {
+        using ndT = ndarray<TypeParam>;
+
+        // Valid shapes
+        ASSERT_NO_THROW(mm(ndT(shape_t({1,})),      ndT(shape_t({1,}))));
+        ASSERT_NO_THROW(mm(ndT(shape_t({3,})),      ndT(shape_t({3,}))));
+        ASSERT_NO_THROW(mm(ndT(shape_t({2, 3})),    ndT(shape_t({3,}))));
+        ASSERT_NO_THROW(mm(ndT(shape_t({4, 2, 3})), ndT(shape_t({3,}))));
+        ASSERT_NO_THROW(mm(ndT(shape_t({2, 3})),    ndT(shape_t({3, 3}))));
+        ASSERT_NO_THROW(mm(ndT(shape_t({1, 2, 3})), ndT(shape_t({3, 1, 3}))));
+        ASSERT_NO_THROW(mm(ndT(shape_t({3,})),      ndT(shape_t({3, 2}))));
+
+        // Wrong shapes
+        ASSERT_THROW(mm(ndT(shape_t({2,})),   ndT(shape_t({1,}))), std::runtime_error);
+        ASSERT_THROW(mm(ndT(shape_t({1,})),   ndT(shape_t({2,}))), std::runtime_error);
+        ASSERT_THROW(mm(ndT(shape_t({3, 3})), ndT(shape_t({2, 4}))), std::runtime_error);
+
+        // Valid output
+        {   // (1,) x (1,) -> (1,)
+            auto A = r_<int>({5,}).template cast<TypeParam>();
+            auto B = r_<int>({3,}).template cast<TypeParam>();
+            auto C_gt = r_<int>({15,}).template cast<TypeParam>();
+            auto C = mm(A, B);
+            ASSERT_EQ(C.shape(), shape_t({1,}));
+            if constexpr (is_int<TypeParam>()) {
+                ASSERT_TRUE(all((C == C_gt).ravel(), 0)[{0}]);
+            } else {
+                ASSERT_TRUE(allclose(C, C_gt));
+            }
+        }
+        {   // (3,) x (3,) -> (1,)
+            auto A = r_<int>({5, 3, 4}).template cast<TypeParam>();
+            auto B = r_<int>({3, 1, 2}).template cast<TypeParam>();
+            auto C_gt = r_<int>({26,}).template cast<TypeParam>();
+            auto C = mm(A, B);
+            ASSERT_EQ(C.shape(), shape_t({1,}));
+            if constexpr (is_int<TypeParam>()) {
+                ASSERT_TRUE(all((C == C_gt).ravel(), 0)[{0}]);
+            } else {
+                ASSERT_TRUE(allclose(C, C_gt));
+            }
+        }
+        {   // (2, 3) x (3,) -> (2,)
+            auto A = (r_<int>({1, 2, 3,
+                               4, 5, 6})
+                      .reshape({2, 3})
+                      .template cast<TypeParam>());
+            auto B = r_<int>({5, 7, 2}).template cast<TypeParam>();
+            auto C_gt = r_<int>({25, 67}).template cast<TypeParam>();
+            auto C = mm(A, B);
+            ASSERT_EQ(C.shape(), shape_t({2,}));
+            if constexpr (is_int<TypeParam>()) {
+                ASSERT_TRUE(all((C == C_gt).ravel(), 0)[{0}]);
+            } else {
+                ASSERT_TRUE(allclose(C, C_gt));
+            }
+        }
+        {   // (4, 2, 3) x (3,) -> (4, 2)
+            auto A = ((arange<int>(0, 4 * 2 * 3, 1) + 1)
+                      .reshape({4, 2, 3})
+                      .template cast<TypeParam>());
+            auto B = r_<int>({-5, 7, -2}).template cast<TypeParam>();
+            auto C_gt = (r_<int>({3, 3,
+                                  3, 3,
+                                  3, 3,
+                                  3, 3})
+                         .reshape({4, 2})
+                         .template cast<TypeParam>());
+            auto C = mm(A, B);
+            ASSERT_EQ(C.shape(), shape_t({4, 2}));
+            if constexpr (is_int<TypeParam>()) {
+                ASSERT_TRUE(all((C == C_gt).ravel(), 0)[{0}]);
+            } else {
+                ASSERT_TRUE(allclose(C, C_gt));
+            }
+        }
+        {   // (2, 3) x (3, 3) -> (2, 3)
+            auto A = ((arange<int>(0, 2 * 3, 1) + 1)
+                      .reshape({2, 3})
+                      .template cast<TypeParam>());
+            auto B = ((arange<int>(0, 3 * 3, 1) + 5)
+                      .reshape({3, 3})
+                      .template cast<TypeParam>());
+            auto C_gt = (r_<int>({ 54,  60,  66,
+                                  126, 141, 156})
+                         .reshape({2, 3})
+                         .template cast<TypeParam>());
+            auto C = mm(A, B);
+            ASSERT_EQ(C.shape(), shape_t({2, 3}));
+            if constexpr (is_int<TypeParam>()) {
+                ASSERT_TRUE(all((C == C_gt).ravel(), 0)[{0}]);
+            } else {
+                ASSERT_TRUE(allclose(C, C_gt));
+            }
+        }
+        {   // (1, 2, 3) x (3, 1, 3) -> (1, 2, 1, 3)
+            auto A = ((arange<int>(0, 2 * 3, 1) + 1)
+                      .reshape({1, 2, 3})
+                      .template cast<TypeParam>());
+            auto B = ((arange<int>(0, 3 * 3, 1) + 5)
+                      .reshape({3, 1, 3})
+                      .template cast<TypeParam>());
+            auto C_gt = (r_<int>({ 54,  60,  66,
+                                  126, 141, 156})
+                         .reshape({1, 2, 1, 3})
+                         .template cast<TypeParam>());
+            auto C = mm(A, B);
+            ASSERT_EQ(C.shape(), shape_t({1, 2, 1, 3}));
+            if constexpr (is_int<TypeParam>()) {
+                ASSERT_TRUE(all((C == C_gt).ravel(), 0)[{0}]);
+            } else {
+                ASSERT_TRUE(allclose(C, C_gt));
+            }
+        }
+        {   // (3,) x (3, 2) -> (2,)
+            auto A = (r_<int>({5, 3, 4})
+                      .template cast<TypeParam>());
+            auto B = (r_<int>({1, 2,
+                               0, 5,
+                               3, 7})
+                      .reshape({3, 2})
+                      .template cast<TypeParam>());
+            auto C_gt = (r_<int>({17, 53})
+                         .template cast<TypeParam>());
+            auto C = mm(A, B);
+            ASSERT_EQ(C.shape(), shape_t({2,}));
+            if constexpr (is_int<TypeParam>()) {
+                ASSERT_TRUE(all((C == C_gt).ravel(), 0)[{0}]);
+            } else {
+                ASSERT_TRUE(allclose(C, C_gt));
+            }
+        }
+
+        // Valid output (buffer provided)
+        {   // (1,) x (1,) -> (1,)
+            auto A = r_<int>({5,}).template cast<TypeParam>();
+            auto B = r_<int>({3,}).template cast<TypeParam>();
+            auto C_gt = r_<int>({15,}).template cast<TypeParam>();
+            ndarray<TypeParam> C(C_gt.shape());
+            auto Z = mm(A, B, &C);
+            ASSERT_TRUE(Z.equals(C));
+            ASSERT_EQ(C.shape(), shape_t({1,}));
+            if constexpr (is_int<TypeParam>()) {
+                ASSERT_TRUE(all((C == C_gt).ravel(), 0)[{0}]);
+            } else {
+                ASSERT_TRUE(allclose(C, C_gt));
+            }
+        }
+        {   // (3,) x (3,) -> (1,)
+            auto A = r_<int>({5, 3, 4}).template cast<TypeParam>();
+            auto B = r_<int>({3, 1, 2}).template cast<TypeParam>();
+            auto C_gt = r_<int>({26,}).template cast<TypeParam>();
+            ndarray<TypeParam> C(C_gt.shape());
+            auto Z = mm(A, B, &C);
+            ASSERT_TRUE(Z.equals(C));
+            ASSERT_EQ(C.shape(), shape_t({1,}));
+            if constexpr (is_int<TypeParam>()) {
+                ASSERT_TRUE(all((C == C_gt).ravel(), 0)[{0}]);
+            } else {
+                ASSERT_TRUE(allclose(C, C_gt));
+            }
+        }
+        {   // (2, 3) x (3,) -> (2,)
+            auto A = (r_<int>({1, 2, 3,
+                               4, 5, 6})
+                      .reshape({2, 3})
+                      .template cast<TypeParam>());
+            auto B = r_<int>({5, 7, 2}).template cast<TypeParam>();
+            auto C_gt = r_<int>({25, 67}).template cast<TypeParam>();
+            ndarray<TypeParam> C(C_gt.shape());
+            auto Z = mm(A, B, &C);
+            ASSERT_TRUE(Z.equals(C));
+            ASSERT_EQ(C.shape(), shape_t({2,}));
+            if constexpr (is_int<TypeParam>()) {
+                ASSERT_TRUE(all((C == C_gt).ravel(), 0)[{0}]);
+            } else {
+                ASSERT_TRUE(allclose(C, C_gt));
+            }
+        }
+        {   // (4, 2, 3) x (3,) -> (4, 2)
+            auto A = ((arange<int>(0, 4 * 2 * 3, 1) + 1)
+                      .reshape({4, 2, 3})
+                      .template cast<TypeParam>());
+            auto B = r_<int>({-5, 7, -2}).template cast<TypeParam>();
+            auto C_gt = (r_<int>({3, 3,
+                                  3, 3,
+                                  3, 3,
+                                  3, 3})
+                         .reshape({4, 2})
+                         .template cast<TypeParam>());
+            ndarray<TypeParam> C(C_gt.shape());
+            auto Z = mm(A, B, &C);
+            ASSERT_TRUE(Z.equals(C));
+            ASSERT_EQ(C.shape(), shape_t({4, 2}));
+            if constexpr (is_int<TypeParam>()) {
+                ASSERT_TRUE(all((C == C_gt).ravel(), 0)[{0}]);
+            } else {
+                ASSERT_TRUE(allclose(C, C_gt));
+            }
+        }
+        {   // (2, 3) x (3, 3) -> (2, 3)
+            auto A = ((arange<int>(0, 2 * 3, 1) + 1)
+                      .reshape({2, 3})
+                      .template cast<TypeParam>());
+            auto B = ((arange<int>(0, 3 * 3, 1) + 5)
+                      .reshape({3, 3})
+                      .template cast<TypeParam>());
+            auto C_gt = (r_<int>({ 54,  60,  66,
+                                  126, 141, 156})
+                         .reshape({2, 3})
+                         .template cast<TypeParam>());
+            ndarray<TypeParam> C(C_gt.shape());
+            auto Z = mm(A, B, &C);
+            ASSERT_TRUE(Z.equals(C));
+            ASSERT_EQ(C.shape(), shape_t({2, 3}));
+            if constexpr (is_int<TypeParam>()) {
+                ASSERT_TRUE(all((C == C_gt).ravel(), 0)[{0}]);
+            } else {
+                ASSERT_TRUE(allclose(C, C_gt));
+            }
+        }
+        {   // (1, 2, 3) x (3, 1, 3) -> (1, 2, 1, 3)
+            auto A = ((arange<int>(0, 2 * 3, 1) + 1)
+                      .reshape({1, 2, 3})
+                      .template cast<TypeParam>());
+            auto B = ((arange<int>(0, 3 * 3, 1) + 5)
+                      .reshape({3, 1, 3})
+                      .template cast<TypeParam>());
+            auto C_gt = (r_<int>({ 54,  60,  66,
+                                  126, 141, 156})
+                         .reshape({1, 2, 1, 3})
+                         .template cast<TypeParam>());
+            ndarray<TypeParam> C(C_gt.shape());
+            auto Z = mm(A, B, &C);
+            ASSERT_TRUE(Z.equals(C));
+            ASSERT_EQ(C.shape(), shape_t({1, 2, 1, 3}));
+            if constexpr (is_int<TypeParam>()) {
+                ASSERT_TRUE(all((C == C_gt).ravel(), 0)[{0}]);
+            } else {
+                ASSERT_TRUE(allclose(C, C_gt));
+            }
+        }
+        {   // (3,) x (3, 2) -> (2,)
+            auto A = (r_<int>({5, 3, 4})
+                      .template cast<TypeParam>());
+            auto B = (r_<int>({1, 2,
+                               0, 5,
+                               3, 7})
+                      .reshape({3, 2})
+                      .template cast<TypeParam>());
+            auto C_gt = (r_<int>({17, 53})
+                         .template cast<TypeParam>());
+            ndarray<TypeParam> C(C_gt.shape());
+            auto Z = mm(A, B, &C);
+            ASSERT_TRUE(Z.equals(C));
+            ASSERT_EQ(C.shape(), shape_t({2,}));
+            if constexpr (is_int<TypeParam>()) {
+                ASSERT_TRUE(all((C == C_gt).ravel(), 0)[{0}]);
+            } else {
+                ASSERT_TRUE(allclose(C, C_gt));
+            }
+        }
+    }
+
+    // Initialize tests ====================================================
+    REGISTER_TYPED_TEST_CASE_P(TestNdLinalgIntFloatComplex,
+                               TestMM);
+    INSTANTIATE_TYPED_TEST_CASE_P(My, TestNdLinalgIntFloatComplex, MyIntFloatComplexTypes);
+}
 
 #endif // TEST_NDLINALG_CPP
